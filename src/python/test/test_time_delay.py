f6669e4679d959e1a367d0753afcb873f2d17826
Marc Glisse <marc.glisse@inria.fr>
2020-02-24 09:44:48 +0100

    Generalize timedelay to vector series of different size

--- src/python/test/test_time_delay.py
+++ src/python/test/test_time_delay.py
@@ -1,6 +1,7 @@
 from gudhi.point_cloud.timedelay import TimeDelayEmbedding
 import numpy as np
 
+
 def test_normal():
     # Sample array
     ts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -34,3 +35,9 @@ def test_normal():
     assert (attractor[0] == np.array([1, 3, 5])).all()
     assert (attractor[1] == np.array([3, 5, 7])).all()
     assert (attractor[2] == np.array([5, 7, 9])).all()
+
+    # Vector series
+    ts = np.arange(0, 10).reshape(-1, 2)
+    prep = TimeDelayEmbedding(dim=4)
+    prep.fit([ts])
+    assert (prep.transform([ts])[0] == [[0, 1, 2, 3], [2, 3, 4, 5], [4, 5, 6, 7], [6, 7, 8, 9]]).all()
