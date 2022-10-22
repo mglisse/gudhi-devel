509af6d8b3bf03cc1c8b63d185d833bda71bd528
Marc Glisse <marc.glisse@inria.fr>
2022-10-22 21:33:45 +0200

    First draft of an alternative version of choose_n_farthest_points, with better asymptotic complexity when the doubling dimension is bounded.

--- src/Subsampling/example/example_choose_n_farthest_points.cpp
+++ src/Subsampling/example/example_choose_n_farthest_points.cpp
@@ -4,27 +4,63 @@
 #include <CGAL/Random.h>
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <iterator>
+#include <chrono>
 
-int main(void) {
+int main(int argc, char**argv) {
   typedef CGAL::Epick_d<CGAL::Dimension_tag<4> > K;
   typedef typename K::Point_d Point_d;
 
-  CGAL::Random rd;
+  CGAL::Random rd(atoi(argv[2]));
+  //CGAL::Random rd(994);
 
+  const int n2 = atoi(argv[1]);
+  //const int n2 = 4;
+  const int n1 = n2;
+  const int j = 0;
   std::vector<Point_d> points;
-  for (int i = 0; i < 500; ++i)
+  for (int i = 0; i < n2; ++i)
     points.push_back(Point_d(rd.get_double(-1., 1), rd.get_double(-1., 1),
                              rd.get_double(-1., 1), rd.get_double(-1., 1)));
 
   K k;
-  std::vector<Point_d> results;
-  Gudhi::subsampling::choose_n_farthest_points(k.squared_distance_d_object(), points, 100,
-                                               Gudhi::subsampling::random_starting_point,
-                                               std::back_inserter(results));
-  std::clog << "Before sparsification: " << points.size() << " points.\n";
-  std::clog << "After  sparsification: " << results.size() << " points.\n";
 
+  //std::cerr.setf(std::ios_base::fixed, std::ios_base::floatfield);
+  //std::cerr.precision(2);
+  //std::cerr << "Matrice de distances" << '\n';
+  //for(int i=0; i<n2; ++i) {
+  //  for(int j=0; j<n2; ++j) {
+  //    std::cerr << k.squared_distance_d_object()(points[i],points[j]) << '\t';
+  //  }
+  //  std::cerr << '\n';
+  //}
+
+  auto dis = [&](auto&p, auto&q){return std::sqrt(k.squared_distance_d_object()(p,q));};
+
+  std::vector<Point_d> results;
+  std::vector<double> dists;
+  auto time_start1 = std::chrono::system_clock::now();
+  Gudhi::subsampling::choose_n_farthest_points1(dis, points, n1,
+                                               j,
+                                               std::back_inserter(results),
+                                               std::back_inserter(dists)
+                                               );
+  auto time_stop1 = std::chrono::system_clock::now();
+  std::ofstream o("O");
+  for(int i=0;i<n1;++i) o << dists[i] << " -- " << results[i] << '\n';
+  results.clear(); dists.clear();
+  auto time_start2 = std::chrono::system_clock::now();
+  Gudhi::subsampling::choose_n_farthest_points(dis, points, n1,
+                                               j,
+                                               std::back_inserter(results),
+                                               std::back_inserter(dists)
+                                               );
+  auto time_stop2 = std::chrono::system_clock::now();
+  std::ofstream n("N");
+  for(int i=0;i<n1;++i) n << dists[i] << " -- " << results[i] << '\n';
+  std::cerr << "Time old " << std::chrono::duration_cast<std::chrono::milliseconds>((time_stop1 - time_start1)).count()
+             << " vs new " << std::chrono::duration_cast<std::chrono::milliseconds>((time_stop2 - time_start2)).count() << '\n';
   return 0;
 }
--- src/Subsampling/include/gudhi/choose_n_farthest_points.h
+++ src/Subsampling/include/gudhi/choose_n_farthest_points.h
@@ -12,11 +12,14 @@
 #define CHOOSE_N_FARTHEST_POINTS_H_
 
 #include <boost/range.hpp>
+#include <boost/heap/skew_heap.hpp>
 
 #include <gudhi/Null_output_iterator.h>
 
 #include <iterator>
 #include <vector>
+#include <set>
+#include <unordered_set>
 #include <random>
 #include <limits>  // for numeric_limits<>
 
@@ -47,7 +50,7 @@ enum : std::size_t {
  *  outputs the distance from each of those points to the set of previous
  *  points in `dist_it`.
  *  \tparam Distance must provide an operator() that takes 2 points (value type of the range)
- *  and returns their distance (or some more general proximity measure) as a `double`.
+ *  and returns their distance as a `double`. It must be a true metric, the algorithm relies on the triangle inequality.
  *  \tparam Point_range Random access range of points.
  *  \tparam PointOutputIterator Output iterator whose value type is the point type.
  *  \tparam DistanceOutputIterator Output iterator for distances.
@@ -66,7 +69,7 @@ template < typename Distance,
 typename Point_range,
 typename PointOutputIterator,
 typename DistanceOutputIterator = Null_output_iterator>
-void choose_n_farthest_points(Distance dist,
+void choose_n_farthest_points1(Distance dist,
                               Point_range const &input_pts,
                               std::size_t final_size,
                               std::size_t starting_point,
@@ -139,6 +142,162 @@ void choose_n_farthest_points(Distance dist,
       }
     *output_it++ = input_pts[points[curr_max_w]];
     *dist_it++ = dist_to_L[curr_max_w];
+  }
+}
+template < typename Distance,
+typename Point_range,
+typename PointOutputIterator,
+typename DistanceOutputIterator = Null_output_iterator>
+void choose_n_farthest_points(Distance dist_,
+                              Point_range const &input_pts,
+                              std::size_t final_size,
+                              std::size_t starting_point,
+                              PointOutputIterator output_it,
+                              DistanceOutputIterator dist_it = {}) {
+  std::size_t nb_points = boost::size(input_pts);
+  if (final_size > nb_points)
+    final_size = nb_points;
+
+  // Tests to the limit
+  if (final_size < 1)
+    return;
+
+  if (starting_point == random_starting_point) {
+    // Choose randomly the first landmark
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<std::size_t> dis(0, nb_points - 1);
+    starting_point = dis(gen);
+  }
+
+//  // FIXME: don't hard-code the type as double. For Epeck_d, we also want to handle types that do not have an infinity.
+//  static_assert(std::numeric_limits<double>::has_infinity, "the number type needs to support infinity()");
+
+  *output_it++ = input_pts[starting_point];
+  *dist_it++ = std::numeric_limits<double>::infinity();
+  if (final_size == 1) return;
+
+  auto dist = [&](std::size_t a, std::size_t b){ return dist_(input_pts[a], input_pts[b]); };
+
+  //struct Landmark_info { double radius; std::vector<std::size_t> voronoi; std::vector<std::size_t> neighbors; typename radius_priority_ds::handle_type position_in_queue};
+  //std::vector<Landmark_info> landmarks(nb_points);
+  //auto compare_landmark_radius = [&](std::size_t a, std::size_t b){ return landmarks[a].radius < landmarks[b].radius; };
+  //typedef boost::heap::skew_heap<std::size_t, boost::heap::compare<decltype(compare_landmark_radius)>, boost::heap::mutable_<true>, boost::heap::constant_time_size<false>> radius_priority_ds;
+  //radius_priority_ds radius_priority(compare_landmark_radius);
+
+  struct Compare_landmark_radius;
+  typedef boost::heap::skew_heap<std::size_t, boost::heap::compare<Compare_landmark_radius>, boost::heap::mutable_<true>, boost::heap::constant_time_size<false>> radius_priority_ds;
+  struct Landmark_info {
+    std::size_t far; double radius;
+    std::vector<std::size_t> voronoi;
+    std::set<std::size_t> neighbors;
+    typename radius_priority_ds::handle_type position_in_queue;
+  };
+  std::vector<Landmark_info> landmarks(nb_points);
+  struct Compare_landmark_radius {
+    std::vector<Landmark_info>* landmarks_p;
+    Compare_landmark_radius(std::vector<Landmark_info>* p): landmarks_p(p) {}
+    bool operator()(std::size_t a, std::size_t b)const{ return (*landmarks_p)[a].radius < (*landmarks_p)[b].radius; }
+  };
+  radius_priority_ds radius_priority(&landmarks);
+  auto compute_radius = [&](std::size_t i)
+  {
+    double r = -std::numeric_limits<double>::infinity();
+    std::size_t jmax = -1;
+    for(std::size_t j : landmarks[i].voronoi) {
+      double d = dist(i,j);
+      if (d > r) {
+        r = d;
+        jmax = j;
+      }
+    }
+    landmarks[i].radius = r;
+    landmarks[i].far = jmax;
+  };
+  auto update_radius = [&](std::size_t i)
+  {
+    compute_radius(i);
+    radius_priority.decrease(landmarks[i].position_in_queue);
+  };
+  {
+    // Initialize everything with starting_point
+    auto& ini = landmarks[starting_point];
+    ini.voronoi.reserve(nb_points - 1);
+    for (std::size_t i = 0; i < nb_points; ++i)
+      if (i != starting_point)
+        ini.voronoi.push_back(i);
+    compute_radius(starting_point);
+    ini.position_in_queue = radius_priority.push(starting_point);
+  }
+  for (std::size_t current_number_of_landmarks = 1; current_number_of_landmarks != final_size; current_number_of_landmarks++) {
+    std::size_t l_parent = radius_priority.top();
+    auto& parent_info = landmarks[l_parent];
+    std::size_t l = parent_info.far;
+    double radius = parent_info.radius;
+    auto& info = landmarks[l];
+    *output_it++ = input_pts[l];
+    *dist_it++ = radius;
+    std::unordered_set<std::size_t> l_neighbors;
+    auto handle_neighbor = [&](std::size_t ngb)
+    {
+      auto& ngb_info = landmarks[ngb];
+      auto it = std::remove_if(ngb_info.voronoi.begin(), ngb_info.voronoi.end(), [&](std::size_t w)
+          {
+            if (dist(l, w) < dist(ngb, w)) {
+              if (w != l)
+                info.voronoi.push_back(w);
+              return true;
+            }
+            return false;
+          });
+      bool modified = (it != ngb_info.voronoi.end());
+      ngb_info.voronoi.erase(it, ngb_info.voronoi.end());
+      if (modified) {
+        if (ngb_info.voronoi.empty()) { // Special-casing this does not seem very useful
+          radius_priority.erase(ngb_info.position_in_queue);
+          for (std::size_t near : ngb_info.neighbors) {
+            // Clean-up, neighbors is lazily updated when we visit it
+            if (landmarks[near].voronoi.size() != 0 && dist(ngb, near) <= 3 * radius) // could be tighter using the radius of ngb and near?
+              l_neighbors.insert(near);
+            // don't prune ngb_info.neighbors, if this is l_parent we still need it, and this is the last time we look at it.
+            // err, we shouldn't clear() it, but removing the neighbors that are too far is still useful for l_parent
+          }
+          // can be done directly in the loop above, once the exact datastructure is settled
+          //ngb_info.neighbors.clear(); // also remove the symmetric?
+        } else {
+          // What if ngb_info.voronoi is now empty?
+          update_radius(ngb); // check if 'far' was removed, otherwise unnecessary
+          std::vector<std::size_t> to_remove;
+          for (std::size_t near : ngb_info.neighbors) {
+            // Clean-up, neighbors is lazily updated when we visit it
+            if (landmarks[near].voronoi.size() != 0 && dist(ngb, near) <= 3 * radius) // could be tighter using the radius of ngb and near?
+              l_neighbors.insert(near);
+            else
+              to_remove.push_back(near);
+          }
+          // can be done directly in the loop above, once the exact datastructure is settled
+          for (std::size_t x : to_remove) {
+            ngb_info.neighbors.erase(x); // also remove the symmetric?
+          }
+        }
+      }
+      return modified;
+    };
+    handle_neighbor(l_parent);
+    for (std::size_t ngb : parent_info.neighbors)
+      handle_neighbor(ngb);
+    //if (parent_info.voronoi.empty()) parent_info.neighbors.clear(); // useless, we will never look at it?
+    compute_radius(l);
+    info.position_in_queue = radius_priority.push(l);
+    // ???
+    l_neighbors.insert(l_parent);
+    // if neighbors is already a set, we don't need an intermediate l_neighbors, or we could make neighbors a vector
+    for (std::size_t ngb : l_neighbors) {
+      if (landmarks[ngb].voronoi.size() != 0 && dist(l, ngb) <= 3 * radius) {
+        info.neighbors.insert(ngb);
+        landmarks[ngb].neighbors.insert(l);
+      }
+    }
   }
 }
 
